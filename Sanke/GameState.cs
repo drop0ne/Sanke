a754dd2d8f5f7b3d5d5573b34f333d7c733372e3
Jake <102178397+drop0ne@users.noreply.github.com>
2024-06-03 21:28:24 -0500

    GameState.cs logic implementation step is complete

--- Sanke/GameState.cs
+++ Sanke/GameState.cs
@@ -1,4 +1,6 @@
-﻿namespace Sanke
+﻿using System.Runtime.CompilerServices;
+
+namespace Sanke
 {
     public class GameState
     {
@@ -62,5 +64,87 @@
             Position position = emptyPositions[random.Next(emptyPositions.Count)];
             Grid[position.Row, position.Col] = GridValue.Food;
         }
+
+        public Position HeadPosition()
+        {
+            return snakePositions.First.Value;
+        }
+
+        public Position TailPosition()
+        {
+            return snakePositions.Last.Value;
+        }
+
+        public IEnumerable<Position> SnakePositions()
+        {
+            return snakePositions;
+        }
+
+        private void AddHead(Position position)
+        {
+            snakePositions.AddFirst(position);
+            Grid[position.Row, position.Col] = GridValue.Snake;
+        }
+        private void RemoveTail()
+        {
+            Position tail = snakePositions.Last.Value;
+            Grid[tail.Row, tail.Col] = GridValue.Empty;
+            snakePositions.RemoveLast();
+        }
+
+        public void changeDirection(Direction direction)
+        {
+            if (Direction.Opposite() == direction)
+            {
+                return;
+            }
+            Direction = direction;
+        }
+
+        private bool OutOfBounds(Position position)
+        {
+            return position.Row < 0 || position.Row >= Rows || position.Col < 0 || position.Col >= Cols;
+        }
+
+        private GridValue CollisionDetection(Position newHeadPos)
+        {
+            if (OutOfBounds(newHeadPos))
+            {
+                return GridValue.outOfBounds;
+            }
+
+            if (newHeadPos == TailPosition())
+            {
+                return GridValue.Empty;
+            }
+
+            return Grid[newHeadPos.Row, newHeadPos.Col];
+        }
+
+        public void Move()
+        {
+            Position newHeadPos = HeadPosition().Translate(Direction);
+
+            GridValue collision = CollisionDetection(newHeadPos);
+
+            if (collision == GridValue.Snake || collision == GridValue.outOfBounds)
+            {
+                GameOver = true;
+                return;
+            }
+            else if (collision == GridValue.Empty)
+            {
+                RemoveTail();
+                AddHead(newHeadPos);
+            }else if (collision == GridValue.Food)
+            {
+                Score++;
+                AddHead(newHeadPos);
+                AddFood();
+            }
+
+        }
+
     }
+
 }
